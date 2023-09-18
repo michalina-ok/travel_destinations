3c5dad5de8c14be915e4787344b863feab49d4af
Michalina <michalina.oniszczuk@gmail.com>
2023-09-18 10:39:28 +0200

    added travel_destinations

---
+++ travel_destinations.js
@@ -0,0 +1,6 @@
+const travel_destinations = [
+    {country: "Japan", place: "Mount Fuji"},
+    {country: "Norway", place: "Geirangerfjord"},
+    {contry: "Australia", place: "Sydney Opera House"},
+   
+]
