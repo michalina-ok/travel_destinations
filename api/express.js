e1dc5aa93b862cdc766229c6c2d85e537055b5d9
Rei Sikk <reix0010@stud.kea.dk>
2023-10-01 17:08:49 +0200

    mongoose breaking stuff

--- api/express.js
+++ api/express.js
@@ -1,73 +1,89 @@
-const express = require('express')
-const app = express()
-const port = 4000
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const Destination = require('../schemas/destination.js');
 
-
-
-let connectionString = 'mongodb://127.0.0.1:27017/'
-
-
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(connectionString,  {
-  serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-  }
-}
-);
-
-const db = client.db("travel_destinations_ola");
-const destCollection = db.collection("destinations");
-
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({extended: true}))
+const app = express();
+const port = 4000;
+let connectionString = 'mongodb://127.0.0.1:27017/';
 //allow request from different port origins
 const corsOptions = {
   origin: 'http://127.0.0.1:5501', // Update this to match your frontend's origin
 };
 
+const options = {
+  origin: ["http://127.0.0.1:5501/form.html", "http://127.0.0.1:5501"],
+  methods: ["GET", "POST"],
+  allowedHeaders: ["X-Requested-With,content-type"],
+};
+
+
+// middleware
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({extended: true}))
 app.use(cors(corsOptions));
-
-
 
 
 //Listen for GET requests
 app.get('/destinations/:destinationId', (req, res) => {
     //it knows what the id is because of the :id on the line above
-  
     res.status(200).json("You requested a destination with an id");
 })
-app.get('/destinations/', async (req, res) => {
-    const result = await destCollection.find().toArray();
-    console.log(result);
-    res.status(200).send(result);
+
+
+
+app.get('/destinations', cors(options),  (req, res) => {
+  mongoose.connect(`${connectionString}`)
+  .then(() => {
+    console.log("connected to mongoDB");
+    Destination.find({}).then(result => {
+        console.log(result);
+        res.status(200).json(result)
+    }).catch(err => {
+        res.status(500).json(err);
     })
+   /*  .finally(() => {
+      console.log("disconnected from mongoDB");
+      mongoose.disconnect();
+    }); */
+  })
+
+})
+
 
 //Listen for POST requests
 app.post('/destination',  async (req, res) => {
   console.log(req.body, "req.body");
+  
+  const destination = new Destination({
+    title: req.body.title,
+    country: req.body.country,
+    arrival_date: req.body.arrival_date,
+    departure_date: req.body.departure_date,
+    image: req.body.image,
+    description: req.body.description,
+    link: req.body.link
+  });
 
-        const doc = {
-           title: req.body.title,
-           country: req.body.country,
-            arrival_date: req.body.arrival_date,
-            departure_date: req.body.departure_date,
-            image: req.body.image,
-            description: req.body.description,
-            link: req.body.link
-         }
-         await destCollection.insertOne(doc);
+  try {
+    const newDestination = await destination.save();
+    console.log(newDestination, "newDestination");
+    res.status(201).json(newDestination);
+  } catch (err) {
+    res.status(500).send({message: err.message});
+  }
+
+ /*  await destCollection.insertOne(destination); */
    
-    res.status(201).send("Destination added");
+   /*  res.status(201).send("Destination added"); */
   })
 
 
-app.listen(port, () => {
-  console.log(`server init at: localhost:${port}`)
-})
+
+  // Start the Express server
+  app.listen(port, () => {
+    console.log(`server init at: localhost:${port}`)
+  });
+
 
  
--- travel_destinations.js
+++ travel_destinations.js
@@ -1,31 +1,51 @@
 import { base64ToImage } from "./utils/base64toImg.js";
 
 window.addEventListener("DOMContentLoaded", async () => {
-  
-  const response = await fetch("http://127.0.0.1:4000/destinations/", {
-   method: "GET",
-   headers: {
-     "Content-type": "application/json",
-     "Access-Control-Allow-Origin": "http://127.0.0.1:4000/destinations/",
-   },
-  })
-  const body = await response.json()
-  const base64value = Object.values(body)[0].image;
-  const imageElement = base64ToImage(base64value);
-  transform(body);
-})
+  try {
+    const response = await fetch("http://127.0.0.1:4000/destinations/", {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "http://127.0.0.1:4000/destinations/",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    const body = await response.json();
+    console.log(body, "body");
+    
+    if (!body || body.length === 0) {
+      console.log("No destinations found");
+      document.querySelector(".destinations-container").innerHTML = "No destinations found";
+      return; // Exit the function if there are no destinations
+    }
+    transform(body);
+    
+    const base64value = Object.values(body)[0]?.image;
+    console.log(base64value, "base64value");
+    if (base64value) {
+      base64ToImage(base64value);
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+});
 
 
 
 function transform(data) {
-  const elements = data.map((x) => {
+  console.log(data, "data in transform function")
+  const elements = Object.values(data).map((x) => {
       const clone = cloneTemplate();
       clone.querySelector(".country").innerText = x.country;
       clone.querySelector(".location").innerText = x.title;
       clone.querySelector(".description").innerText = x.description;
       clone.querySelector(".arrival-date").innerText = dayjs(x.arrival_date).format('DD MMMM, YYYY');
       clone.querySelector(".leave-date").innerText = dayjs(x.departure_date).format('DD MMMM, YYYY');
-      clone.querySelector(".destination_img").src = x.image;
+     /*  clone.querySelector(".destination_img").src = x.image; */
       if (x.link === "") {
         clone.querySelector(".google-maps").remove();
       } else {
@@ -43,5 +63,4 @@ function transform(data) {
     return clone;
   }
   
-  window.addEventListener("load", transform)
   
