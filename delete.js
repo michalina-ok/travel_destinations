48b170a2487e74febe399717c090dab570242912
Michalina <michalina.oniszczuk@gmail.com>
2023-10-04 17:32:44 +0200

    added buttons

---
+++ delete.js
@@ -0,0 +1,7 @@
+const deleteButton = document.querySelector(".delete-button")
+
+deleteButton.addEventListener("click", deleteEntry)
+
+function deleteEntry() {
+    console.log("Entry deleted - lool not really")
+}
